027f0c53db12b7705081908584b848a22cc5f6b2
Paulo Oliveira Da Silva <pauloo@amdocs.com>
2019-07-30 15:53:42 -0300

    Adding tests

--- employee_manager/tests.py
+++ employee_manager/tests.py
@@ -1,3 +1,26 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from .models import Employee
+from rest_framework import status
 
-# Create your tests here.
+
+# These tests works without authentication and permissions
+class EmployeeAPITest(APITestCase):
+    def setUp(self):
+        pass
+
+    # Testing employee creation.
+    def test_create_emplooyee(self):
+        employee = {
+            'name': 'Paulo Henrique',
+            'email': 'pauloo@amdocs.com',
+            'department': 'Backend Developer'
+        }
+
+        response = self.client.post('/employee/',employee,format='json') 
+    
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+    # Testing employee list. 
+    def test_retrieve_employee(self):
+        response = self.client.get('employee/1')
+        body = response
+        print(body)
